00eaa8b823f22cad9db9fd303e7e41b97009641a
Arun Godwin Patel <arungodwinpatel@googlemail.com>
2021-05-04 18:19:06 +0100

    feature: adding Transaction & Block classes

---
+++ index.ts
@@ -0,0 +1,29 @@
+import * as crypto from 'crypto'
+
+class Transaction {
+    constructor(
+        public amount: number,
+        public payer: string, // public key
+        public payee: string  // public key
+    ) {}
+
+    toString() {
+        return JSON.stringify(this);
+    }
+}
+
+class Block {
+    constructor(
+        public prevHash: string,
+        public transaction: Transaction,
+        public ts = Date.now()
+    ) {}
+
+    get hash() {
+        const str = JSON.stringify(this);
+        const hash = crypto.createHash('SHA256');
+        hash.update(str).end();
+        return hash.digest('hex')
+    }
+}
+
